2e43ba7fcc74b135747b8b697a42f501de26d853
Ricardo Santos <RicardoSantos.Developer@gmail.com>
2014-01-07 17:34:38 +0000

    Base Version
    This version is from “Device - HID - Joystick” example provided in
    “microchip_solutions_v2013-06-15”

---
+++ Firmware/MPLAB.X/build/LPC_USB_Development_Kit_PIC16F1459/production/_ext/926206843/usb_hal_pic24.p1.d
@@ -0,0 +1,12 @@
+ build/LPC_USB_Development_Kit_PIC16F1459/production/_ext/926206843/usb_hal_pic24.d  \
+ build/LPC_USB_Development_Kit_PIC16F1459/production/_ext/926206843/usb_hal_pic24.p1:  \
+ ../../../../Microchip/USB/usb_hal_pic24.c  \
+../../../../Microchip/Include/USB/usb_hal_pic16f1.h  \
+../usb_config.h  \
+../../../../Microchip/Include/USB/usb_hal.h  \
+../../../../Microchip/Include/USB/usb_common.h  \
+../../../../Microchip/Include/Compiler.h  \
+../../../../Microchip/Include/GenericTypeDefs.h  \
+../../../../Microchip/Include/USB/usb.h  \
+../../../../Microchip/Include/USB/usb_device.h  \
+../../../../Microchip/Include/USB/usb_ch9.h 
